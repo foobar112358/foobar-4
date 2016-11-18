61a34e5b27a9ca69155faf9ae5eb4ae1d1bb0d62
mwx@rmbp <mwx36mwx@163.com>
2016-11-19 02:05:10 +0800

    ci

--- bomb_baby.py
+++ bomb_baby.py
@@ -6,20 +6,29 @@ def answer(M, F):
     gen = 0
     possible = False
     while M > 0 and F > 0:
+        print M, F
         if M == 1 and F == 1:
             possible = True
+            break
+        elif M == F:
             break
         if M < F:
             M, F = F, M
 
-        M = M - F
-        gen += 1
+        i = 1
+        while M - F * i > 0:
+            i *= 2
+        i /= 2
+
+        M = M - F * i
+        gen += i
 
     if not possible:
         return 'impossible'
     else:
         return str(gen)
 
-M = '3'
-F = '100000'
+M = '1231231231212312414'
+F = '10000000000000000000000000000000000000000000000000000000000000000000'
+
 print answer(M, F)
