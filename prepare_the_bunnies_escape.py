2a583b089505f231c5308fd76b7c4d0d9ec4284d
mwx@rmbp <mwx36mwx@163.com>
2016-11-18 22:59:14 +0800

    wow

--- prepare_the_bunnies_escape.py
+++ prepare_the_bunnies_escape.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python
 from collections import deque
 
-def bfs(maze):
-    q = deque([(0,0,1)])
-    vis = [[False for _ in xrange(len(maze[0]))] for _ in xrange(len(maze))]
-    vis[0][0] = True
+def bfs(maze, sx, sy):
+    M, N = len(maze), len(maze[0])
+    score = [[None for _ in xrange(N)] for _ in xrange(M)]
 
+    cnt = 0
+    q = deque([(sx, sy, 1)])
     while q:
+        cnt += 1
         x, y, k = q.popleft()
-        vis[x][y] = True
-        if x == len(maze) - 1  and y == len(maze[0]) - 1:
-            return k
-        for i in [-1, 0, 1]:
-            for j in [-1, 0, 1]:
-                if abs(i * j) == 1:
-                    continue
-                nx, ny = x + i, y + j
-                if 0 <= nx < len(maze) and 0 <= ny < len(maze[0]):
-                    if not vis[nx][ny] and maze[nx][ny] == 0:
-                        q.append((nx, ny, k+1))
-    return 2 << 32
+        if score[x][y] != None:
+            continue
+
+        score[x][y] = k
+        if maze[x][y] == 1:
+            continue
+
+        for i, j in [[1,0], [-1,0], [0, 1], [0, -1]]:
+            nx, ny = x + i, y + j
+            if 0 <= nx < M and 0 <= ny < N:
+                if score[nx][ny] == None:
+                    q.append((nx, ny, k+1))
+
+    return score
 
 def answer(maze):
-    ans = bfs(maze)
+    M, N = len(maze), len(maze[0])
+    a1 = bfs(maze, 0, 0)
+    a2 = bfs(maze, M - 1, N - 1)
 
-    # for i in xrange(len(maze)):
-    #     for j in xrange(len(maze[0])):
-    #         if maze[i][j] == 1:
-    #             maze[i][j] = 0
-    #             ans = min(bfs(maze), ans)
-    #             maze[i][j] = 1
+    ans = 2 << 32
+    for i in xrange(M):
+        for j in xrange(N):
+            if a1[i][j] and a2[i][j]:
+                ans = min(a1[i][j] + a2[i][j] - 1, ans)
     return ans
 
 
-print answer([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 1, 0], [1, 1, 1, 0]])
+size = 20
+print answer([[0 for _ in xrange(size)] for _ in xrange(size)])
