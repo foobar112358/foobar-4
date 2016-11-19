2973ef9a09ac6814ef8d1cb285e6362372135e58
mwx@rmbp <mwx36mwx@163.com>
2016-11-19 08:10:31 +0800

    refine

--- the_grandest_staircase_of_them_all.py
+++ the_grandest_staircase_of_them_all.py
@@ -1,34 +1,80 @@
 #!/usr/bin/env python
 
-# todo, can be more percise
+"""
+The Grandest Staircase Of Them All
+==================================
+
+With her LAMBCHOP doomsday device finished, Commander Lambda is preparing for her debut on the galactic stage - but in order to make a grand entrance, she needs a grand staircase! As her personal assistant, you've been tasked with figuring out how to build the best staircase EVER.
+
+Lambda has given you an overview of the types of bricks available, plus a budget. You can buy different amounts of the different types of bricks (for example, 3 little pink bricks, or 5 blue lace bricks). Commander Lambda wants to know how many different types of staircases can be built with each amount of bricks, so she can pick the one with the most options.
+
+Each type of staircase should consist of 2 or more steps.  No two steps are allowed to be at the same height - each step must be lower than the previous one. All steps must contain at least one brick. A step's height is classified as the total amount of bricks that make up that step.
+For example, when N = 3, you have only 1 choice of how to build the staircase, with the first step having a height of 2 and the second step having a height of 1: (# indicates a brick)
+
+#
+##
+21
+
+When N = 4, you still only have 1 staircase choice:
+
+#
+#
+##
+31
+
+But when N = 5, there are two ways you can build a staircase from the given bricks. The two staircases can have heights (4, 1) or (3, 2), as shown below:
+
+#
+#
+#
+##
+41
+
+#
+##
+##
+32
+
+Write a function called answer(n) that takes a positive integer n and returns the number of different staircases that can be built from exactly n bricks. n will always be at least 3 (so you can have a staircase at all), but no more than 200, because Commander Lambda's not made of money!
+
+Languages
+=========
+
+To provide a Python solution, edit solution.py
+To provide a Java solution, edit solution.java
+
+Test cases
+==========
+
+Inputs:
+    (int) n = 3
+Output:
+    (int) 1
+
+Inputs:
+    (int) n = 200
+Output:
+    (int) 487067745
+
+Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
+"""
+
 def answer(n):
     dp = [[0 for _ in xrange(n)] for _ in xrange(n + 1)]
-    dp[3][2] = 1
 
-    for j in [0, 1, 2]:
+    for j in xrange(3):
         for i in xrange(j, n):
             dp[j][i] = 1
 
     for i in xrange(3, n + 1):
         for bot in xrange(2, n):
-            if i == 3 and bot == 2:
-                continue
-            if bot > i:
-                dp[i][bot] = dp[i][bot - 1]
-            else:
-                dp[i][bot] = dp[i][bot-1] + dp[i - bot][bot - 1]
-
-            # print '(%d, %d)[%d] = (%d, %d)[%d] + (%d, %d)[%d]' % (i, bot,dp[i][bot], i, bot - 1,dp[i][bot-1], i -
-            #                                                       bot, bot - 1,
-            #                                                       dp[i-bot][bot -
-            #                                                               1])
-
-    # for i in dp:
-    #     print i
-    # print
+            dp[i][bot] = dp[i][bot-1]
+            if bot <= i:
+                dp[i][bot] += dp[i-bot][bot-1]
 
     return dp[n][n-1]
 
 
-#for i in xrange(3, 10):
-print answer(200)
+if __name__ == '__main__':
+    for i in xrange(3, 201):
+        print answer(i)
